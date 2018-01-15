0a7e264e56927441e1249beef0f3364855e135b0
Toya <toya.dapena@gmail.com>
2018-01-15 19:50:21 +0100

    Añado scss

---
+++ js/wo-js.js
@@ -0,0 +1,14 @@
+
+
+var swiper;
+swiper = new Swiper('.swiper-container', {
+    navigation: {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        height:900,
+        noSwiping: 'true'
+    },
+});
+
+    $('.owl-carousel').owlCarousel();
